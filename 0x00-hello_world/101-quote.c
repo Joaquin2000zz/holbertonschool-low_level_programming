c43512d207c7771aab010545a3682a756f8a3464
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-09-30 11:02:49 -0700

    Write a C program that prints something, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Write a C program that prints something
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
