59ed319797ce645f0eb5834e42d11e163bc91074
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-09-30 08:00:45 -0700

    adding some changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/** 
+/**
 * main - excecuting a puts to print a text output
 * Return: Programming is like building a multilingual puzzle
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	   
+	puts("\"Programming is like building a multilingual puzzle")
 	return (0);
 }
