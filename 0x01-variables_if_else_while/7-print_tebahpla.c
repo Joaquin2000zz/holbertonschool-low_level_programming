4bee077150d321f30fa6cca9572bc999f7ce10d2
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-01 13:35:47 -0700

    creating a for to show the alphabet in reverse but in ASCII

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * * main - for
+ * * Return: the alphabet
+ * */
+
+int main(void)
+{
+	int alphabet;
+
+	for (num = 122 ; num <= 97 ; num--)
+	putchar(num);
+	putchar('\n');
+
+	return (0);
+}
