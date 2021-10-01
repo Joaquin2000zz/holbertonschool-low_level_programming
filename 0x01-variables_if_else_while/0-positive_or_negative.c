401c1d1f986fe2a7b1fb2fa4e332fd41e762f2f6
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-01 09:51:49 -0700

    adding the excersise 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > void)
+	if (n > 0)
 	{
 	printf("%d is positive\n", n);
 	}
-	else if (n == void)
+	else if (n == 0)
 	{
 	printf("%d is zero\n", n);
 	}
