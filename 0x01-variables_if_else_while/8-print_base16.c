537196dd1b51ddb10ce64d0236e27fff10abce96
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-01 13:45:59 -0700

    creating a for to count in hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - for
+* Return: counting in hexa
+*/
+
+int main(void)
+{
+	int hexa;
+
+	for (hexa = 0 ; hexa <= 16 ; hexa++)
+	printf("%x", hexa);
+	putchar('\n');
+
+	return (0);
+}
