12a193559060012ad6c2d5acdca38b95279349bf
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-01 10:14:10 -0700

    adding the excersise 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	last= n % 10;
 	if (n % 10 > 5)
 	{
-	printf("Last digit of %d and is grater than 5",n);
+	printf("Last digit of %d and is grater than 5",n,last);
 	}
 	else if (n % 10 == 0)
 	{
-	printf("Last digit of %d and is 0",n);
+	printf("Last digit of %d and is 0",n,last);
 	}
 	else if (n % 10 < 6 && !0)
 	{
-	printf("Last digit of %d and is less than 6 and not 0",n);
+	printf("Last digit of %d and is less than 6 and not 0",n,last);
 	}
 	return (0);
 }
