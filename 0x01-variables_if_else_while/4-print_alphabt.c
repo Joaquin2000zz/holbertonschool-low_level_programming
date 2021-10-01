f468ec8734e1bc97e4cba2827ebf04808be2d458
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-01 11:47:22 -0700

    creating a for to show the alphabet in lowwer and uppercases

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@ int main(void)
 	char alphabet;
 	char n = '\n';
 
-	for (alphabet = 'a' ; alphabet <= 'z' alphabet++);
+	for (alphabet = 'a' alphabet <= 'z' alphabet++);
 	{
 	if (alphabet = !'q' | !'e');
 	{
 	putchar(alphabet);
+	}
+	}
 	putchar(n);
-	}
-	}
 	return (0);
 }
