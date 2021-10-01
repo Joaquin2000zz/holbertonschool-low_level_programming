921d5f3d4d90bb7878cbae4bb30cd6e6c6d34d38
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-01 10:56:14 -0700

    creating a for to show the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - for 
+* return: the alphabet
+*/
+
+int main(void)
+
+{
+	char alphabet = "a";
+
+	for (alphabet = "a" ; ch <= "z" ; alphabet++)
+	putchar(alphabet);
+	
+	return (0);
+}
+
