1d19725a0823766ff2dea3b2b8add4b940300034
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-01 12:21:27 -0700

    creating a for to count

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - for
+* Return: the alphabet
+*/
+
+int main(void)
+{
+	int num;
+	char n = '\n'
+	for (num = 1 ; num <= 9 ; num++)
+	printf("%d"num);
+	putchar(n);
+
+	return (0);
+}
