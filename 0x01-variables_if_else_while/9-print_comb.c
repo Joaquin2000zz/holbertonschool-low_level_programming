c674e3da8eaf9b50940f8b4cada354ebf7640b39
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-01 14:01:39 -0700

    making a do while to combinate numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - for
+* Return: counting
+*/
+
+int main(void)
+{
+	int num, i;
+
+	while (i <= num 0){
+/*	if (num%i == 0){*/
+	printf("%d", num);
+	break;
+/*	}*/
+	i++;
+	}
+
+	return (0);
+}
