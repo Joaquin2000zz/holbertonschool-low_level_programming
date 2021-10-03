dc5879248725b465dd01fc7e4d9d628a3119235a
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-03 15:05:46 -0700

    a program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+* main - for
+* Return: counting
+*/
+
+int main(void)
+{
+	int i;
+	int ii;
+	int iii;
+	int iiii;
+
+	for (iiii = 48 ; iiii <= 57 ; iiii++)
+	{
+	for (iii = 48 ; iii <= 56 ; iii++)
+	{
+	for (ii = 48 ; ii <= 57 ; ii++)
+	{
+	for (i = 49 ; i <= 57 ; i++)
+	{
+	if (iii < i && iii < ii && ii < i)
+	{
+	putchar(iiii);
+	putchar(iii);
+	putchar(ii);
+	putchar(i);
+	}
+	if (iiii && iii < ii && i != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
