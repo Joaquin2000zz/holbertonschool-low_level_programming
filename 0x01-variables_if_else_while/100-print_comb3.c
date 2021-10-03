4f1bd5e21c67b5cd4160a30c4a0856ce5f0787f0
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-02 18:10:27 -0700

    trying to make a nested loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,14 @@
 
 int main(void)
 {
-	int;
+	int i = 0;
 	
 	for( i == 49 ; i <= 56 ; i++ )
 	{
 	for( i == 48 ; i <= 57 ; i++ )
 	if( i == 49 )
 	{
-	putchar(i)	
+	putchar(i);	
 	}
 	else if( i == 50 && i = 57 )
 	{
