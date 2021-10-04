0db81cdfc9e29887e155d21293d479b00ccc520d
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-04 09:45:58 -0700

    trying to solve the 102 one

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,22 +11,22 @@ int main(void)
 
 	for (ii = 0 ; ii <= 99 ; ii++)
 	{
-	for (i = ii ; ii <= 99 ; i++)
-	{
-	if (ii != i)
-	{
-	putchar(((ii/10)%10)+48);
-	putchar(((ii/10)%10)+48);
-	putchar(' ');
-	putchar(((ii/1)%10)+48);
-	putchar(((ii/1)%10)+48);
-	}
-	if (ii != 57 && i != 56 && ii != i)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
+		for (i = ii ; i <= 99 ; i++)
+		{
+			if (ii != i)
+			{
+				putchar(((ii/10)%10)+48);
+				putchar(((ii)%10)+48);
+				putchar(' ');
+				putchar(((ii/10)%10)+48);
+				putchar(((ii)%10)+48);
+			}
+			if (ii != 57 && i != 56 && ii != i)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 
