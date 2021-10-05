9aa760eec1c71a6c81d2f0bbeff66a6a69183b44
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-05 09:30:08 -0700

    adding the funtion to print the alphabetx

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - use a for to print the alphabet
+*
+* Return: Always 0.
+*/
+int print_alphabet(char alphabet)
+{
+alphabet = 'a';
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	putchar(alphabet);
+	putchar('\n');
+}
