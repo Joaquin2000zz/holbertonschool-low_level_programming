cacdd76e8069073627db71de0e089193e345eb44
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-05 15:25:01 -0700

    Write a function that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,15 +12,16 @@ void jack_bauer(void)
 
 	for (hours = 0 ; hours < 24; hours++)
 	{
-	if (hours <= 23 && minutes <= 59)
-	{
-	_putchar(hours + 48);
-	_putchar(hours + 48);
-	_putchar(' ');
-	_putchar(minutes + 48);
-	_putchar(minutes + 48);
-	for (minutes = hours ; minutes < 60; minutes++)
-	{
+		for (minutes = hours ; minutes < 60; minutes++)
+		{
+			if (hours <= 23 && minutes <= 59)
+			{
+				_putchar(hours + 48);
+				_putchar(hours + 48);
+				_putchar(' ');
+				_putchar(minutes + 48);
+				_putchar(minutes + 48);
+	
 	}
 	}	
 	}
