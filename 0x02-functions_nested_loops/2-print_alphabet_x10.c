84ace50c63f809947bd4bb7c8a85b4072f99749e
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-05 10:38:43 -0700

    showing the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,5 +10,7 @@ void print_alphabet_x10(void)
 	int i;
 
 	for(i=0 ; i < 11 ; i++)
-	print_alphabet();
+		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+			_putchar(alphabet);
+	_putchar('\n');
 }
