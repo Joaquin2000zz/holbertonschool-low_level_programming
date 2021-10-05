3591b5ab9642afad4b4491de0558e761e8faa767
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-05 09:24:09 -0700

    making an alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,10 @@
 *
 * Return: Always 0.
 */
-int print_alphabet(void)
+int print_alphabet(char alphabet)
 {
-char alphabet = 'a';
-
+alphabet = 'a'
 	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 	putchar(alphabet);
 	putchar('\n');
-	
-	return (0);
 }
