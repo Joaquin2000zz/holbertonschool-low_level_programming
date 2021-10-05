ca5d2e195642ad5f944bc1434a4fccf3d01ecfac
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-05 09:31:47 -0700

    solving problems

---
+++ 0x01-variables_if_else_while/intento_fallido_del_102.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+* main - for
+* Return: counting
+*/
+
+int main(void)
+{
+	int i;
+	int ii;
+	int iii;
+	int iiii;
+
+	for (iiii = 48 ; iiii <= 57 ; iiii++)
+	{
+	for (iii = 48 ; iii <= 56 ; iii++)
+	{
+	for (ii = 48 ; ii <= 57 ; ii++)
+	{
+	for (i = 49 ; i <= 57 ; i++)
+	{
+	if (iii != i)
+	{
+	putchar(iiii);
+	putchar(iii);
+	putchar(' ');
+	putchar(ii);
+	putchar(i);
+	}
+	if (iiii != 57 && iii != 56 && iiii != i && iii != i)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
