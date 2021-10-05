866542ecb503c15e1e397a57e2c2a72a39849aee
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-05 10:52:31 -0700

    showing the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,9 @@ void print_alphabet_x10(void)
 	int alphabet;
 
 	for(i=0 ; i <= 10 ; i++)
-	{	_putchar('\n');
+	{
 		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 			_putchar(alphabet);
+			_putchar('\n');
 	}
 }
