dd1a0128b39ad3799c5fda17d714a327847186ac
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-05 09:57:02 -0700

    making a funtion that prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - use a for to print the alphabet
-* 
+* print_alphabet - use a for to print the alphabet 
 * Return: Always 0.
 */
 void print_alphabet(void)
