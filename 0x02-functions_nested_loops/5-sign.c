bdacab189ba6105815146cc8fde25a912fb7b2e5
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-05 11:49:30 -0700

    Write a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _isalpha - check the code.
+*@c: boolean value that define if the input is a lowercase or not
+* Return: Always 0.
+*/
+
+int print_sign(int n)
+{
+
+	if (n >= 49 && c <= 57)
+	{
+		return (1);
+	}
+	else if (n == 49)
+	{
+		return (0);
+	}
+	else if (n < 49)
+	{
+		return (-1);
+	}
+}
