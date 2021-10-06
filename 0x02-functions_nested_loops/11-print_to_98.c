3108ca393297a182e5d79a43b309eeda3ffe0a2e
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-06 15:01:36 -0700

    couting to reach 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,14 @@ void print_to_98(int i)
 {
 	if (i <= 98)
 	{
-	for (i = 0 ; i <= 98 ; i++)
+	for (i ; i <= 98 ; i++)
 	{
 	printf("%d, ", i);
 	}
 	}
 	if (i >= 98)
 	{
-	for (i = 0 ; i <= 98 ; i--)
+	for (i ; i <= 98 ; i--)
 	{
 	printf("%d, ", i);
 	}
