b31a59f1df6fea9ab8cadf9b2bd5ec70c07cb55e
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-06 16:48:08 -0700

    infinite loop avoidided

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-/**	int i;
-*
-*	printf("Infinite loop incoming :(\n");
-*
-*	i = 0;
+/**	int i; */
+
+	printf("Infinite loop incoming :(\n");
+/**
+*	i = 0; this while is infinite because doesn't have a incremental value
 *
 *	while (i < 10)
 *	{
