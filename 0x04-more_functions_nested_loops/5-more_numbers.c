fa02c5371368ee275bb7c2e5d672e8fb7d27613a
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-07 10:35:57 -0700

    Write a function that prints the numbers, from 0 to 14, followed by a new line.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints numbers from 0 to 9 but exlude the 2 and the 4
+* more_numbers - prints numbers from 0 to 9 but exlude the 2 and the 4
 *
 * Return: Always 0.
 */
-void print_most_numbers(void)
+void more_numbers(void)
 {
 	char y;
 	char x;
 	
-	for (y = 0 ; x <= 10 ; y++)
+	for (y = 0 ; y <= 10 ; y++)
 	{
 	for (x = 0 ; x <= 14 ; x++)
 	{
@@ -18,11 +18,9 @@ void print_most_numbers(void)
 	{
 		_putchar(((x / 10) % 10) + 48);
 	}
-	else
-	{
 		_putchar((x % 10) + 48);
+
 	}
-	}
-	_putchar('\n')
+	_putchar('\n');
 	}
 }
