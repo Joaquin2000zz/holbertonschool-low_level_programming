1c04f6cc930dbbca76513d34b2baa4f08baafe2e
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-08 10:50:00 -0700

    The “Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 	int x;
 	for (x = 1; x <= 100; x++)
 	{
-		if (((x % 3) == 0) && ((x % 5) == 0))
+		if ((x % 3) == 0 && (x % 5) == 0)
 		{
 		printf("FizzBuzz");
 		}
@@ -23,7 +23,7 @@ int main(void)
 		{
 		printf("Buzz");
 		}
-		else if (((x % 3) != 0) || (((x % 3) != 0) && (x != 100)))
+		else if (((x % 3) != 0) || (((x % 5) != 0) && (x != 100)))
 		{
 		printf("%d ", x);
 		}
