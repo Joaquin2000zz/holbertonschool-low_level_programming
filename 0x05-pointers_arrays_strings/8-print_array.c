037d891f190c0a703d81785321a99534540acc1e
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-13 18:30:00 -0700

    Write a function that prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; a[n] != '\0'; i++)
-			;		
-	for (i = 0; a[i] <= n; i++)
-		printf("%d, ", a[i]);
-	putchar('\n');
+	for (i = 0; a[i] != 0 && i <= (n - 1); i++)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	printf("\n");
 }
