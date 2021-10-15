fd0edb363b9405712905c98635c346283d41ee00
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-15 09:50:31 -0700

    adding all the excercises

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcpy - a funtion that copy strings
+* @dest: the variable that saves the copied string
+* @src: the variable that brings the string that you need to copy
+* @n: the variable that determine the length of the string
+* Return: src copied in dest
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, valresult;
+
+	i = 0;
+
+	while (s1[i] != s2[i])
+	{
+		valresult = s1[i] - s2[i];
+		i++;
+	}
+	return (valresult);
+}
