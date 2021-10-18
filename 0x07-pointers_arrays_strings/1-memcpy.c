b1bc6a2f319c58ff318ac5feb7a53ad940904bdd
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-18 09:07:24 -0700

    Write a function that copies memory area.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,22 +2,22 @@
 
 /**
 * _memset - prints buffer in hexa
-* @s: the address of memory to print
-* @b: the size of the memory to print
-* @n: 
-* Return: Nothing.
+* @dest: the address of the string to fill whith n pasted bytes
+* @src: the chars to copy on dest
+* @n: the number of bytes to be copied in src
+* Return: the chars copied from src to paste in dest.
 */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 	
 	i = 0;
 
-	while (s[i] != 0 && i < n)
+	while (i < n)
 	{
-		s[i] = b;
+		dest[i] = src[i];
 		i++;
 	}
-	return (s);
+	return (dest);
 }
