f23fc1a5bffc32636ab559f8d7ee7faafe8838ec
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-18 18:13:11 -0700

    Write a function that locates a substring.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,36 +2,38 @@
 
 /**
 * _strstr - check the code
-*@haystack: 
-*@needle:
-* Return: Always 0.
+* @haystack: string to compare chars each other to show the the equal substring
+* @needle: the string that you use to compare with the main char
+* Return: the substring founded in the comparsion into haystack and needle
 */
 
-char * _strstr(char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle)
 {
 	int p1 = 0, p2 = 0;
 
 	if (*needle == 0)
+	{
 		return (haystack);
+	}
 	else
-	{	
+	{
 		while (haystack[p1])
 		{
-			while(needle[p2] && haystack[p1] == needle[0])
+			while (needle[p2] && haystack[p1] == needle[0])
 			{
 				if (haystack[p2 + p1] == needle[p2])
 					p2++;
 				else
 					break;
 			}
-	
+
 		if (needle[p2])
 		{
 			p1++;
 			p2 = 0;
 		}
 		else
-			return (haystack + p1);	
+			return (haystack + p1);
 		}
 	}
 		return (0);
