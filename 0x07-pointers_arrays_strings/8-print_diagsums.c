c1344387b7ef8b682f9b73bc7eb08f587883c0c9
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-19 19:13:39 -0700

    making a addition of the two diagonals of a matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* print_diafsums - print the addition within the values of the diagonals of a matrix
+* print_diafsums - addition within the values of the diagonals of a matrix
 * @a: rows of the matrix
 * @size: the columns of the matrix
 * Return: 0.
@@ -12,10 +12,10 @@ void print_diagsums(int *a, int size)
 {
 	int i, n1 = 0, n2 = 0;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		n1 = n1 + a[(i * (size + 1))];
 		n2 = n2 + a[(size - 1) + (i * (size - 1))];
 	}
-	printf("%d, %d\n", n1, n2);
+	printf("%d, %d \n", n1, n2);
 }
