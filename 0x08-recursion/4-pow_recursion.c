6bef8d834241ba63af90275c09feb3d8a4685898
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-20 12:58:46 -0700

    adding files of excercise 4 that makes the power of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _pow_recursion - writes the character c to stdout
- * @x: The character to print
- * @y: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _pow_recursion - make a power of a number
+ * @x: number to do the power
+ * @y: times to do the power
+ * Return: the calculation
  */
 int _pow_recursion(int x, int y)
 {
