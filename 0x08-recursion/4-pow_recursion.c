0a336d8d4da152bfe4880d1b93c9549a0fcbd83b
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-20 12:44:29 -0700

    calculate the power of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _pow_recursion - make the power of x y times
  * @x: value to do to do the power
  * @y: y times that you do the power
  * Return: the calculation
  */
+
 int _pow_recursion(int x, int y)
 {
 	if (y > 0)
