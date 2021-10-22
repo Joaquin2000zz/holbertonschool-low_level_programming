4f7a2c7820f6f80001a8b660e9289b42a4ccf994
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-22 09:03:31 -0700

    making a funtion that print the name of the funtionx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main: "print the name of the program"
+ * main- "print the name of the program"
  * @argc: "the long of the argv"
  * @argv: "array of strings that have the arguments of the program"
- * return: always 0
+ * Return: always 0
  */
-int main (int argc __attribute__((unused)), char *argv[])
-{
-	printf ("%s\n",argv[0]);
 
-	return 0;
+int main(int argc__attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
