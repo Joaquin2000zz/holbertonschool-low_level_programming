4b7ca51fe14ae871488b3ea6baf0ea089902c29c
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-22 09:32:54 -0700

    adding files of excercise 2 that prints all arguments it recives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Write a program that prints all arguments it receives.
+* @argc: variable with the long of the argv
+* @argv: array that have the arguments of the program
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		 printf("%s\n", argv[i]);
+
+	return (0);
+}
