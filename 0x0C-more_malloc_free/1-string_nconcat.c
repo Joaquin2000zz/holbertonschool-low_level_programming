e728cdff2c322946f3d42e5b1f745ca5b9ffb85e
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-10-28 11:08:08 -0700

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,19 @@
-#include <string.h>
 #include <stdlib.h>
+
+/**
+ * _strlen - count the large of a string
+ *
+ * Return: Always 0.
+ */
+
+unsigned int _strlen(char *c unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; c[i] != 0)
+		;
+	return (i);
+}
 
 /**
  * string_nconcat - function that concatenates two strings.
@@ -8,25 +22,25 @@
  * @n: the first n characters of the second string that you gonna concatenate
  * Return: the concatenated string.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n )
 {
 	unsigned int i = 0, j = 0;
 	char *ar;
 	
-/**	if (s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
 
  	if (s2 == NULL)
 		s2 = "";
-*/
-		ar = malloc((strlen(s1) + n + 1) * sizeof(char));
+
+		ar = malloc((_strlen(s1) + n + 1) * sizeof(char));
 
 	if (ar == 0)
 		return (0);
 
-	while (i < (strlen(s1) + strlen(s2) + 1))
+	while (i < (_strlen(s1) + _strlen(s2) + 1))
 	{
-		if (i < strlen(s1))
+		if (i < _strlen(s1))
 			ar[i] = s1[i];
 		else if (j < n + 1)
 		{
@@ -39,3 +53,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	return (ar);
 }
+
+
