f4ec1a207dc2d1496fd0a8c5a9e2d3fcca11cda5
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-01 06:51:45 -0700

    making my first structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - struct maded to to get a list with information about doggos :)
+ * @name: doggos's name
+ * @age: doggo's age
+ * @owner: doggo's best friend
+ *
+ * Description: all you need to know about a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
