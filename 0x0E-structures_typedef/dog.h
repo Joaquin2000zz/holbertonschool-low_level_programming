c3a7d480e7896c15b7351ee647674620acb74d5f
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-01 12:12:46 -0700

    function that prints a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,14 @@
  *
  * Description: all you need to know about a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
