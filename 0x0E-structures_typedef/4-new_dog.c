87ef4769227c3718b538b0b51aed18820571e72b
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-01 15:47:12 -0700

    function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,30 +2,35 @@
 #include "dog.h"
 
 /**
- * _strdup - make a pointer to a newly allocated space in memory
- * @str: a pointer that brings the string to print
- * Return: Always 0.
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: string to copy
+ *
+ * Return: *ptr to the duplicate string or NULL if it failed
  */
 char *_strdup(char *str)
 {
-	unsigned int i = 0, j = 0;
-	char *ar = 0;
+	unsigned int pos, len;
+	char *dup;
 
-	if (str == 0)
-		return (0);
+	if (!str)
+		return (NULL);
 
-	while (str[i] != 0)
-		i++;
-	ar = malloc(i + 1 * sizeof(char));
-	if (ar == 0)
-		return (0);
+	for (len = 0; str[len] != 0;)
+		len++;
 
-	while (j < i)
+	dup = (char *)malloc((sizeof(char) * len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (pos = 0; pos <= len; pos++)
 	{
-		ar[j] = str[j];
-		j++;
+		dup[pos] = str[pos];
 	}
-	return (ar);
+
+	return (dup);
 }
 /**
  * new_dog - asign values of a structure in allocated space
