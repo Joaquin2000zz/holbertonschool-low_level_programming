65f8fe3c3ca3cc35c5346ffd1e155426e9e95006
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-01 17:19:24 -0700

    dogs in freedom

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - free dogs (?
+ * @d: the pointer whith the values of the dog
+ * Return: the doggo in freedom.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
