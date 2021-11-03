effa34935a5ced000c3f9f06301d3717314ac6d7
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-03 07:28:45 -0700

    Write a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - print name
+ * @name: name of the person
+ * @f: pointer to a function
+ * Return: the name.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
