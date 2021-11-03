a70d94efba0485a7941b941158b3abc078d890a7
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-03 15:15:03 -0700

    a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,6 +14,9 @@ int main(int argc, char *argv[])
 	if (argc == 4)
 	{
 		printf("%d\n", (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+		return (0);
 	}
-	return (0);
+	printf("Error\n");
+
+	return (98);
 }
