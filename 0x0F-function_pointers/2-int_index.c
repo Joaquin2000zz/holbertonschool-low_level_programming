152b191040b32f9991158441d5e00ebf1344bf38
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-03 10:57:25 -0700

    Write a function that searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+
+/**
+ * int_index - check if a number is equal to 98
+ * @array: array to iterate
+ * @size: size of elements of the array
+ * @cmp: function to compare digits
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array && size <= 0 && !cmp)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) ==  1)
+			return (i);
+	}
+	return (-1);
+}
