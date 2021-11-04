5a2c2dc68c905e67282a67e351bf898660b8e538
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-04 10:29:36 -0700

    print a list of numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - print numbers with a variadic function
+ * @n: n numbers you gonna print
+ * @separator: literally the separator
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num_list;
+	unsigned int i, j = 0, num = 0;
+
+	if (!separator || !n)
+		exit;
+
+	va_start(num_list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(num_list, int);
+		printf("%d", num);
+		if (i < (n - 1))
+			printf("%s", separator);
+	}
+	putchar('\n');
+
+}
