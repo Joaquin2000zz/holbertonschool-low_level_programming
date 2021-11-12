aef281e13c0e3869ba5ed64ac21f06d889ea4782
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-11 18:16:12 -0800

    function that prints a message before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * myStartupFun - funtion that prints text first than the main function
+ * Return: void function
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
