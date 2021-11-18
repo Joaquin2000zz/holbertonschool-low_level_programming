4b989caa0797bce0e9d7e0126eb9ccad2304e9c1
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-11-18 10:25:43 -0800

    function that prints the binary representation of a number.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,22 +8,21 @@
 
 void print_binary(unsigned long int n)
 {
-	long int i = 0;
+	long int i = 63, flag = 0;
 
 	if (n == 0)
 		_putchar('0');
 	else
 	{
-		while ((n >> i) != 0)
-		{
-			i++;
-		}
-		i--;
 		while (i >= 0)
 		{
+
 			if ((n >> i) & 1)
+			{
 				_putchar(1 + 48);
-			else
+				flag++;
+			}
+			else if (flag > 0)
 				_putchar(48);
 			i--;
 		}
