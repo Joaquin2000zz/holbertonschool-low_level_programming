a493d29f1f1d1d6188076ddb8f0ca66122c0d083
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-08 07:37:52 -0800

    adding a condittion to avoid enter to dhe while if the list has a single node

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,13 +12,11 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (!h)
 		return (EXIT_FAILURE);
-	if (!h->next)
-		printf("%d\n", h->n);
 	while (h)
 	{
-		i++;
 		printf("%d\n", h->n);
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
