32d428ea79d891e5f542a267f0884a7e6a5249d3
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-09 06:17:05 -0800

    changed the logic in one if

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	size_t len = 0;
 
-	if (!new && !*h)
+	if (!new && !*h && !h)
 	{
 		free(new);
 		return (NULL);
