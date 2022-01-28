d3d11b82e031f05618bc8f6e0c4b0e305d708882
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-28 05:35:21 -0800

    changing calloc for malloc and asking if the size is 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		if (!newHashTable)
 			return (NULL);
 	newHashTable->size = size;
-	newHashTable->array = calloc(0, sizeof(hash_node_t *) * size);
+	newHashTable->array = malloc(sizeof(hash_node_t *) * size);
 	if (!newHashTable->array)
 	{
 		free(newHashTable);
