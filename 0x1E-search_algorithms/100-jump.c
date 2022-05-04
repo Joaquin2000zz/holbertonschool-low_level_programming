58137218d41a3b5e4e538c00865cf699245808bf
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-04 15:08:28 -0700

    fixed output error and border cases

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,24 @@
 #include "search_algos.h"
-#include <math.h>
+/**
+ * heron- method which calculeates the srqt of a number
+ * @p: number to find their square root
+ * Return: the square root of p
+ */
+double heron(double p)
+{
+	double x = p, xn = 0.5 * p;
+	int i = 0;
+
+	while (1)
+	{
+		xn = 0.5 * (xn + (p / xn));
+		if (x == xn)
+			break;
+		x = xn;
+		i++;
+	}
+	return (xn);
+}
 
 /**
  * jump_search - search a number in sub vectors. O(sqrt(n))
@@ -11,19 +30,21 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-	int square = (int)sqrt((float)size), jump = 0, i;
+	int square = (int)heron((float)size), jump = 0, i;
 
 	if (!array)
 		return (-1);
 	while (1)
 	{
+		printf("Value checked array[%d] = [%d]\n", jump, array[jump]);
 		if (jump <= (int)size || jump == 0)
 		{
-			if (jump + square > value)
+			if (jump + square >= value)
 			{
 				printf("Value found between indexes [%d] and [%d]\n", jump, jump + square);
-				for (i = jump; i < (-jump + (int)size); i++)
+				for (i = jump; i < (int)size; i++)
 				{
+					printf("Value checked array[%d] = [%d]\n", i, array[i]);
 					if (array[i] == value)
 						return ((size_t)i);
 				}
@@ -31,7 +52,6 @@ int jump_search(int *array, size_t size, int value)
 		}
 		else
 			break;
-		printf("Value checked array[%d] = [%d]\n", jump, array[jump]);
 		jump += square;
 	}
 	return (-1);
